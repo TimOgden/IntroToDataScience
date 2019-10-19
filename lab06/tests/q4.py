91b8d0d69c63864a75c3ca81bd9315d24603f8a0
Tim Ogden <togden1999@gmail.com>
2019-10-19 14:59:21 -0400

    Updated labs and homeworks

---
+++ lab06/tests/q4.py
@@ -0,0 +1,24 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(valid_ts) == list
+          True
+          >>> 3 not in valid_ts
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
