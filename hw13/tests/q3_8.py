d04b170b4ff4a4dc3873f05e4e06438346969afb
unknown <togden1999@gmail.com>
2019-12-05 22:38:28 -0500

    Haven't pushed my work in a while.

---
+++ hw13/tests/q3_8.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 3_8',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure next_wait is a single value
+          >>> isinstance(next_wait, float)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ lab10/tests/q2_3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q2_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> sampled_SD_question == 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
